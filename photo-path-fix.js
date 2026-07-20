a6edfa65cb2d61afa97427272db9e8c59d68a150
samyc93 <61203728+samyc93@users.noreply.github.com>
2026-07-20 14:02:51 +0100

    Load first and third photos directly from GitHub

---
+++ photo-path-fix.js
@@ -0,0 +1,2 @@
+p.photos[0].file='https://raw.githubusercontent.com/samyc93/wedding-sibling-game/main/image1-portugal.jpg';
+p.photos[2].file='https://raw.githubusercontent.com/samyc93/wedding-sibling-game/main/image3-beach2008.jpg';
